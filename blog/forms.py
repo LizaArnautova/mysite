e3f8a49f0705380b3d82782158639d21fa280091
Арнаутова Елизавета <90194042+LizaArnautova@users.noreply.github.com>
2022-06-13 14:53:39 +0300

    Add files via upload

---
+++ blog/forms.py
@@ -0,0 +1,14 @@
+from .models import Comment
+from django import forms
+from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
+
+
+# Apply summernote to specific fields.
+class SomeForm(forms.Form):
+    foo = forms.CharField(widget=SummernoteWidget())  # instead of forms.Textarea
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
